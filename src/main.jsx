59d2934dc56f8d2329a926ed6997d4ae32c18fba
Umair Ayub <46655787+umairayub79@users.noreply.github.com>
2023-07-11 01:33:31 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { ToastProvider } from './context/ToastContext'
 import ReactGA from "react-ga4";
 
 ReactGA.initialize(import.meta.env.VITE_GA4_ID)
-console.log(import.meta.env.VITE_GA4_ID)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ToastProvider>
